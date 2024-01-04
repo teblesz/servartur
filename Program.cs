50bc435c6b6126b5f00942b0ef2c7ff4e3d7c588
teblesz <mteblesz@gmail.com>
2024-01-04 01:46:58 +0100

    Add swagger

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.OpenApi.Models;
 using servartur;
 using servartur.Entities;
 
@@ -8,9 +9,17 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Servartur API",
+        Description = "An ASP.NET Core Web API for managing server side of Fluttartur - a local multiplayer game based on \"The Resistance: Avalon\"",
+    });
+});
 builder.Services.AddDbContext<GameDbContext>();
 builder.Services.AddScoped<DbSeeder>();
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 var app = builder.Build();
 
@@ -23,8 +32,8 @@ seeder.Seed();
 // Configure the HTTP request pipeline. //middleware
 if (app.Environment.IsDevelopment())
 {
-    //app.UseSwagger();
-    //app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
