cc81abc92fee749d765ef09835254ff8a3916fcd
teblesz <mteblesz@gmail.com>
2024-01-05 15:21:57 +0100

    Move ListExtensionShuffle to Utils

--- Services/MatchupService.cs
+++ Services/MatchupService.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Query.Internal;
 using servartur.Entities;
 using servartur.Models;
+using servartur.Utils;
 
 namespace servartur.Services;
 
@@ -96,26 +97,5 @@ public class MatchupService : IMatchupService
             teamAssignmentList.RemoveAt(0);
         }
         _dbContext.SaveChanges();
-    }
-}
-public static class ListExtensionShuffle
-{
-    /// <summary>
-    /// Fisher-Yates shuffle for lists
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="list"></param>
-    public static void Shuffle<T>(this List<T> list)
-    {
-        Random rng = new Random();
-        int n = list.Count;
-        while (n > 1)
-        {
-            n--;
-            int k = rng.Next(n + 1);
-            T value = list[k];
-            list[k] = list[n];
-            list[n] = value;
-        }
     }
 }
---
+++ Utils/ListExtensionShuffle.cs
@@ -0,0 +1,23 @@
+﻿namespace servartur.Utils;
+
+public static class ListExtensionShuffle
+{
+    /// <summary>
+    /// Fisher-Yates shuffle for lists
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="list"></param>
+    public static void Shuffle<T>(this List<T> list)
+    {
+        Random rng = new Random();
+        int n = list.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = rng.Next(n + 1);
+            T value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
+    }
+}
