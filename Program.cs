368f2e634035df5bed2d39d0446dd9a6dc781adc
teblesz <mteblesz@gmail.com>
2024-01-06 16:52:46 +0100

    Add NLog to app builder

--- Program.cs
+++ Program.cs
@@ -2,46 +2,75 @@ using Microsoft.OpenApi.Models;
 using servartur;
 using servartur.Entities;
 using servartur.Services;
+using NLog;
+using NLog.Web;
 
-var builder = WebApplication.CreateBuilder(args);
+// Early init of NLog to allow startup and exception logging, before host is built
+var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Debug("init main");
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
+try
 {
-    options.SwaggerDoc("v1", new OpenApiInfo
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add services to the container.
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(addSwaggerOptions());
+    builder.Services.AddDbContext<GameDbContext>();
+    builder.Services.AddScoped<DbSeeder>();
+    builder.Services.AddAutoMapper(typeof(Program).Assembly);
+    builder.Services.AddScoped<IMatchupService, MatchupService>();
+
+    // NLog: Setup NLog for Dependency injection
+    builder.Logging.ClearProviders();
+    builder.Host.UseNLog();
+
+    // Build
+    var app = builder.Build();
+
+    // Call services
+    var scope = app.Services.CreateScope();
+    var seeder = scope.ServiceProvider.GetRequiredService<DbSeeder>();
+    seeder.Seed();
+
+
+    // Configure the HTTP request pipeline. //middleware
+    if (app.Environment.IsDevelopment())
     {
-        Title = "Servartur API",
-        Description = "An ASP.NET Core Web API for managing server side of Fluttartur - a local multiplayer game based on \"The Resistance: Avalon\"",
-    });
-});
-builder.Services.AddDbContext<GameDbContext>();
-builder.Services.AddScoped<DbSeeder>();
-builder.Services.AddAutoMapper(typeof(Program).Assembly);
-builder.Services.AddScoped<IMatchupService, MatchupService>();
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
 
-var app = builder.Build();
+    app.UseHttpsRedirection();
 
-// Call services
-var scope = app.Services.CreateScope();
-var seeder = scope.ServiceProvider.GetRequiredService<DbSeeder>();
-seeder.Seed();
+    //app.UseAuthorization();
 
+    app.MapControllers();
 
-// Configure the HTTP request pipeline. //middleware
-if (app.Environment.IsDevelopment())
+    app.Run();
+}
+catch (Exception exception)
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    // NLog: catch setup errors
+    logger.Error(exception, "Stopped program because of exception");
+    throw;
+}
+finally
+{
+    // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+    NLog.LogManager.Shutdown();
 }
 
-app.UseHttpsRedirection();
-
-//app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+static Action<Swashbuckle.AspNetCore.SwaggerGen.SwaggerGenOptions> addSwaggerOptions()
+{
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    return options =>
+    {
+        options.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Title = "Servartur API",
+            Description = "An ASP.NET Core Web API for managing server side of Fluttartur - a local multiplayer game based on \"The Resistance: Avalon\"",
+        });
+    };
+}
